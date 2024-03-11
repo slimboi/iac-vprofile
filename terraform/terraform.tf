e56751ca552a40c51ab143b94a21affe2649dad5
Ola Fagbule <ola.fagbule@gmail.com>
2024-03-11 17:31:05 +0000

    update clusterName and bucket in terraform and variables file

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -27,9 +27,9 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "gitopsterrastate"
+    bucket = "terraform-state-12285"
     key    = "terraform.tfstate"
-    region = "us-east-2"
+    region = "eu-west-2"
   }
 
   required_version = "~> 1.6.3"
